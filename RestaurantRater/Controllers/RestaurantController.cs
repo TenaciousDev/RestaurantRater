6eebaa1707ce6864eee519e9b5e281246ba76e88
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-11-09 16:06:59 -0500

    built out Details view & controller method

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -79,9 +80,23 @@ namespace RestaurantRater.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Entry(restaurant).State = System.Data.Entity.EntityState.Modified;
+                _db.Entry(restaurant).State = EntityState.Modified;
                 _db.SaveChanges();
                 return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        //GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
             }
             return View(restaurant);
         }
